fd327bc38b9870ca0fe4177e2da8246202274055
Alexander Ruppel <alexander.ruppel@al-chemical.com>
2012-08-11 21:21:14 -0400

    Fixed shitting on loading
    it's so smooth now

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -1,9 +1,15 @@
 class HelloWorldController < ApplicationController
 
 	def index 
+		if params[:input] 
+			process_name
+		else
+			@name = "Enter a String"
+		end			
+	end
+private #tells everything after ruby is private and can only be access from this controller
+	def process_name
 		lower_case_name = params[:input].downcase
-	
-	
 		if lower_case_name == "alex" 
 			@name = "Howdy"
 		elsif lower_case_name == "fitz"
@@ -11,7 +17,7 @@ class HelloWorldController < ApplicationController
 		else
 			@name = "What are you staring at"
 		end
-
+		Rails.logger.info "Lower case name was: " + lower_case_name				
 	end
 
 end
